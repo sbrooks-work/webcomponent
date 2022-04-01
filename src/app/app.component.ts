10a8920908b2ebd30ddaede541c8006202d3a484
sbrooks-work <sbrooks@resurgent.com>
2022-04-01 14:39:14 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ang12base';
+  data: Object[];
+
+  constructor() {
+    this.data = [
+      { key: 'Red' },
+      { key: 'Green' },
+      { key: 'Blue' },
+      { key: 'Orange' },
+      { key: 'Purple' },
+    ];
+  }
 }
